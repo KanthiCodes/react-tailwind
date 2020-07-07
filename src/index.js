a15d2de89bd9f190c8798801a3907226f3d98148
KanthiCodes <kanthi.kriyakara@gmail.com>
2020-07-08 01:45:42 +1000

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/tailwind.output.css';
-
-import App from './components/App';
-
-ReactDOM.render(<App/>,document.querySelector("#root"));
