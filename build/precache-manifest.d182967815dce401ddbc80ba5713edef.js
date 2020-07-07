777f229e938b433a30fa9005f671a45f400b5bc3
KanthiCodes <kanthi.kriyakara@gmail.com>
2020-07-08 02:03:47 +1000

    added build folder

---
+++ build/precache-manifest.d182967815dce401ddbc80ba5713edef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997fa5c16afa8034aac1a58667833181",
+    "url": "/xpo/index.html"
+  },
+  {
+    "revision": "528206a93a52a7efe102",
+    "url": "/xpo/static/css/main.0236fc86.chunk.css"
+  },
+  {
+    "revision": "b7609a4d9e004e2422a8",
+    "url": "/xpo/static/js/2.69f78bf5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/xpo/static/js/2.69f78bf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "528206a93a52a7efe102",
+    "url": "/xpo/static/js/main.179f7781.chunk.js"
+  },
+  {
+    "revision": "c51a6f08149a5487f1d5",
+    "url": "/xpo/static/js/runtime-main.c8eda8c5.js"
+  },
+  {
+    "revision": "196e2e8764b6ee9cf8ac436cb94b957e",
+    "url": "/xpo/static/media/XPO-Logo.196e2e87.svg"
+  },
+  {
+    "revision": "82aeea770a74835cd73213a4b2c3e4ee",
+    "url": "/xpo/static/media/hero.82aeea77.jpg"
+  }
+]);
