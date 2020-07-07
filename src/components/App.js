0fbf88c44f1153f4e17596bc5b4be6f9a53b0e6a
KanthiCodes <kanthi.kriyakara@gmail.com>
2020-07-07 12:37:21 +1000

    Setup tailwindcss
    Setup tailwindcss, postcss, autoprefixer and build scripts

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../img/XPO-Logo.svg'
+
+const App = ()=>{
+    return (
+       <div className="px-5 py-5">
+           <img className="h-10" src={logo} alt="XPO Logo"></img>
+       </div>
+      );
+}
+
+export default App;
